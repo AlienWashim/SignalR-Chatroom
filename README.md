9fac82ffd5dc99dbbed5be0b0c4693d76fcf3fba
Washim Akram <90306771+AlienWashim@users.noreply.github.com>
2024-09-08 10:22:06 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SignalR-Chatroom
+ A real-time chatroom built with ASP.NET Core and SignalR, providing dynamic message updates and a clean user interface without user authentication or message history.
---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+ 
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public async Task JoinChat(string name)
+        {
+            await Clients.All.addNewMessageToPage($"{name} has joined the chat.", true);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string name = Context.QueryString["name"];
+            Clients.All.addNewMessageToPage($"{name} has left the chat.", false);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
